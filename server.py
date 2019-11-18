a2b5bf4d80f2943d6b98334040d8590de5cabfcc
ltisera <lucastisera@gmail.com>
2019-11-18 15:38:16 -0300

    Seguimos sin nada

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, jsonify, request
+import requests
+#Fixer IO presonal api key
+#
+#38a0f63c483d5b0b1819e315606fb6aa
 
 app = Flask(__name__)
 
@@ -6,10 +10,56 @@ app = Flask(__name__)
 def inicialPagina():
     return render_template('index.html')
 
+@app.route("/cotizar", methods=["POST"])
+def cotizar():
+    uri = "http://data.fixer.io/api/latest?access_key=38a0f63c483d5b0b1819e315606fb6aa&symbols=USD,AUD,CAD,PLN,MXN,ARS"
+    quedato = requests.get(uri)
+    return quedato.json()
+
+"""
+Api RESTFul
+"""
+@app.route('/test', methods=['POST'])
+def cargarUsuario():
+    print("Estoy dentro de mi test")
+    return "409",200
+
+@app.route('/nuevoUsuario', methods=['POST'])
+def nuevoUsuario():
+    print("print de request")
+    print(request)
+    print("print de request.data")
+    print(request.data)
+    print("print de request.values")
+    print(request.values)
+    return "409",200
+
+@app.route('/ajaxUsuario', methods=['POST'])
+def ajaxUsuario():
+    print("AJAX")
+    print("print de request")
+    print(request)
+    print("print de request.data")
+    print(request.data)
+    print("print de request.values")
+    print(request.values)
+    return "409",200
+
+@app.route('/vanillaUsuario', methods=['POST'])
+def vanillaUsuario():
+    print("VANILLA")
+    print("print de request")
+    print(request)
+    print("print de request.data")
+    print(request.data)
+    print("print de request.values")
+    print(request.values)
+    return "409",200
+
 """
 Devuelve Archivos que se encuentren dentro de la carpeta static
 /static/<dir>/nombreDeArchivo.ext
-"""
+""" 
 
 @app.route('/static/<path:path>')
 def sirveDirectorioSTATIC(path):
@@ -26,5 +76,10 @@ def sirveDirectorioSTATIC(path):
     directorio = "static/" + directorio
     return send_from_directory(directorio, arc)
 
+@app.route('/favicon.ico', methods=['GET'])
+def devolveFavicon():
+    return send_from_directory("static/img", "favicon.ico")
 
-app.run()
+#
+
+app.run(debug=True)
